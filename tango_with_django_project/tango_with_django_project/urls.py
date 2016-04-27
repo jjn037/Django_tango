197e2fb9a8bb00271777c61d402a85bba1f65460
jjn037 <jiangjianan037@126.com>
2016-04-27 16:33:08 +0800

    rango,work

---
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -0,0 +1,38 @@
+"""tango_with_django_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url, patterns
+from django.contrib import admin
+from rango.views import index, category
+from django.conf import settings # New Import
+from django.conf.urls.static import static # New Import
+
+
+urlpatterns = [
+    url(r'^$', index, name='index'),
+    # url(r'^rango/', include('rango.urls')),
+    url(r'^rango/category/(?P<category_name_slug>[\w\-]+)/$', category, name='category'), # New!
+    url(r'^admin/', include(admin.site.urls)),
+]
+
+if not settings.DEBUG:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
